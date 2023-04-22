1ff4aba4bf6cb9b1f498540740bee33a43c1b407
valery <becwal@yandex.ru>
2023-04-23 01:06:49 +0300

    Создание представления уроков курса и передача туда параметров курса из сессии

--- app/Http/Controllers/LessonscrudController.php
+++ app/Http/Controllers/LessonscrudController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Course;
 use Illuminate\Http\Request;
 
 class LessonscrudController extends Controller
 {
     public function index(Request $request)
     {
-        $course= $request->session()->get('course');
-        dd('уроки курса '.$course);
+        $course_id= $request->session()->get('course');
+        $course = Course::find($course_id);
+         return view('teachers.lessons.index', compact('course'));
     }
 }
---
+++ resources/views/teachers/lessons/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.layout')
+
+@section('content')
+
+уроки курса {{$course->title}}
+
+@endsection
