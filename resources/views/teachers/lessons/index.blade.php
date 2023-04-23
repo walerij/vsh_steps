a07734f5913d8b8dae2c01f670f71cb1fe394e93
valery <becwal@yandex.ru>
2023-04-23 08:55:06 +0300

    Дизайн представления уроков курса (CRUD)

--- resources/views/teachers/lessons/index.blade.php
+++ resources/views/teachers/lessons/index.blade.php
@@ -4,4 +4,27 @@
 
 уроки курса {{$course->title}}
 
+<table class="table table-hover">
+    <thead>
+        <tr>
+            <th>Заголовок</th>
+            <th>Информация</th>
+            <th>Управление</th>
+        </tr>
+    </thead>
+    <tbody>
+    @foreach($course->lessons as $lesson)
+        <tr>
+            <td>{{$lesson->title}}</td>
+            <td>{{$lesson->info}}</td>
+            <td>
+                <a href="#" class="btn btn-dark">
+                    Изменить
+                </a>
+            </td>
+        </tr>
+    @endforeach
+    </tbody>
+</table>
+
 @endsection
