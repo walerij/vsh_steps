5afd194277fc53ba0056f6ec2303fde9d1409ed2
valery <becwal@yandex.ru>
2023-05-12 06:31:46 +0300

    Создание контроллера шагов StepsController и роутов video и quest

---
+++ app/Http/Controllers/StepsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Step;
+use Illuminate\Http\Request;
+
+class StepsController extends Controller
+{
+    public function video(Step $step)
+    {
+        return view();
+    }
+}
--- routes/steps.php
+++ routes/steps.php
@@ -9,3 +9,6 @@ Route::get('/teachers/steps/create',  [App\Http\Controllers\StepscrudController:
 Route::post('/teachers/steps', [App\Http\Controllers\StepscrudController::class,"store"])->name('teachers.steps.store');
 Route::get('/teachers/steps/{step}/createcontent', [App\Http\Controllers\StepscrudController::class,"createcontent"])->name('teachers.steps.createcontent');
 Route::post('/teachers/steps/{step}/', [App\Http\Controllers\StepscrudController::class,"storecontent"])->name('teachers.steps.storecontent');
+
+Route::post('/steps/{step}/video', [App\Http\Controllers\StepsController::class,"video"])->name('steps.video');
+Route::post('/steps/{step}/quest', [App\Http\Controllers\StepsController::class,"quest"])->name('steps.quest');
