e2fc0e5ab6b0c85a2122e104d23ce1c0fca47ac2
valery <becwal@yandex.ru>
2023-06-18 07:06:58 +0300

    Редизайн таб-панелей курсов, консолей комментариев в личном кабинете

--- resources/views/auth/showuserLTE.blade.php
+++ resources/views/auth/showuserLTE.blade.php
@@ -18,13 +18,13 @@
                                     <b>Звание</b> <a class="float-right">{{$user->rank}}</a>
                                 </li>
                                 <li class="list-group-item">
-                                    <b>Накомленный опыт</b> <a class="float-right">{{$user->experience}}</a>
+                                    <b>Накопленный опыт</b> <a class="float-right">{{$user->experience}}</a>
                                 </li>
                                 <li class="list-group-item">
                                     <b>Баланс мегахешей</b> <a class="float-right">{{$user->megahash}}</a>
                                 </li>
                             </ul>
-                            <a href="#" class="btn btn-primary btn-block"><b>Подробнее</b></a>
+
                         </div>
 
                     </div>
@@ -51,6 +51,7 @@
                             <hr>
                             <strong><i class="far fa-file-alt mr-1"></i> Клуб формулистов</strong>
                             <p class="text-muted">не состоит</p>
+                            <a href="#" class="btn btn-primary btn-block"><b>Редактировать данные</b></a>
                         </div>
 
                     </div>
@@ -62,21 +63,72 @@
                         <div class="card-header p-2">
                             <ul class="nav nav-pills">
                                 <li class="nav-item"><a class="nav-link active" href="#mycourses" data-toggle="tab">Курсы</a></li>
-                                <li class="nav-item"><a class="nav-link" href="#timeline" data-toggle="tab">Консольные задачи</a></li>
-                                <li class="nav-item"><a class="nav-link" href="#settings" data-toggle="tab">Комментарии</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#myconsole" data-toggle="tab">Консольные задачи</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#mycomments" data-toggle="tab">Комментарии</a></li>
                             </ul>
                         </div>
                         <div class="card-body">
+
                             <div class="tab-content">
                                 <div class="active tab-pane" id="mycourses">
+                                    <!--первая таблица - курсы которые прошел--->
+                                    <form class="form-horizontal">
+                                        <div class="form-group row">
+                                            <label for="inputName" class="col-sm-2 col-form-label">Name</label>
+                                            <div class="col-sm-10">
+                                                <input type="email" class="form-control" id="inputName" placeholder="Name">
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
+                                            <div class="col-sm-10">
+                                                <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label for="inputName2" class="col-sm-2 col-form-label">Name</label>
+                                            <div class="col-sm-10">
+                                                <input type="text" class="form-control" id="inputName2" placeholder="Name">
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label for="inputExperience" class="col-sm-2 col-form-label">Experience</label>
+                                            <div class="col-sm-10">
+                                                <textarea class="form-control" id="inputExperience" placeholder="Experience"></textarea>
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label for="inputSkills" class="col-sm-2 col-form-label">Skills</label>
+                                            <div class="col-sm-10">
+                                                <input type="text" class="form-control" id="inputSkills" placeholder="Skills">
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <div class="offset-sm-2 col-sm-10">
+                                                <div class="checkbox">
+                                                    <label>
+                                                        <input type="checkbox"> I agree to the <a href="#">terms and conditions</a>
+                                                    </label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <div class="offset-sm-2 col-sm-10">
+                                                <button type="submit" class="btn btn-danger">Submit</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                    <!--конец первой таблицы - курсы которые прошел--->
+                                </div>
+                                <div class="tab-pane" id="mycomments">
 
                                     <div class="post">
                                         <div class="user-block">
                                             <img class="img-circle img-bordered-sm" src="../../dist/img/user1-128x128.jpg" alt="user image">
                                             <span class="username">
-<a href="#">Jonathan Burke Jr.</a>
-<a href="#" class="float-right btn-tool"><i class="fas fa-times"></i></a>
-</span>
+                                            <a href="#">Jonathan Burke Jr.</a>
+                                            <a href="#" class="float-right btn-tool"><i class="fas fa-times"></i></a>
+                                            </span>
                                             <span class="description">Shared publicly - 7:30 PM today</span>
                                         </div>
 
@@ -165,24 +217,24 @@
                                             <a href="#" class="link-black text-sm mr-2"><i class="fas fa-share mr-1"></i> Share</a>
                                             <a href="#" class="link-black text-sm"><i class="far fa-thumbs-up mr-1"></i> Like</a>
                                             <span class="float-right">
-<a href="#" class="link-black text-sm">
-<i class="far fa-comments mr-1"></i> Comments (5)
-</a>
-</span>
+                                            <a href="#" class="link-black text-sm">
+                                            <i class="far fa-comments mr-1"></i> Comments (5)
+                                            </a>
+                                            </span>
                                         </p>
                                         <input class="form-control form-control-sm" type="text" placeholder="Type a comment">
                                     </div>
 
                                 </div>
 
-                                <div class="tab-pane" id="timeline">
+                                <div class="tab-pane" id="myconsole">
 
                                     <div class="timeline timeline-inverse">
 
                                         <div class="time-label">
-<span class="bg-danger">
-10 Feb. 2014
-</span>
+                                            <span class="bg-danger">
+                                            10 Feb. 2014
+                                            </span>
                                         </div>
 
 
@@ -259,54 +311,7 @@
                                     </div>
                                 </div>
 
-                                <div class="tab-pane" id="settings">
-                                    <form class="form-horizontal">
-                                        <div class="form-group row">
-                                            <label for="inputName" class="col-sm-2 col-form-label">Name</label>
-                                            <div class="col-sm-10">
-                                                <input type="email" class="form-control" id="inputName" placeholder="Name">
-                                            </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
-                                            <div class="col-sm-10">
-                                                <input type="email" class="form-control" id="inputEmail" placeholder="Email">
-                                            </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label for="inputName2" class="col-sm-2 col-form-label">Name</label>
-                                            <div class="col-sm-10">
-                                                <input type="text" class="form-control" id="inputName2" placeholder="Name">
-                                            </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label for="inputExperience" class="col-sm-2 col-form-label">Experience</label>
-                                            <div class="col-sm-10">
-                                                <textarea class="form-control" id="inputExperience" placeholder="Experience"></textarea>
-                                            </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label for="inputSkills" class="col-sm-2 col-form-label">Skills</label>
-                                            <div class="col-sm-10">
-                                                <input type="text" class="form-control" id="inputSkills" placeholder="Skills">
-                                            </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <div class="offset-sm-2 col-sm-10">
-                                                <div class="checkbox">
-                                                    <label>
-                                                        <input type="checkbox"> I agree to the <a href="#">terms and conditions</a>
-                                                    </label>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <div class="offset-sm-2 col-sm-10">
-                                                <button type="submit" class="btn btn-danger">Submit</button>
-                                            </div>
-                                        </div>
-                                    </form>
-                                </div>
+
 
                             </div>
 
